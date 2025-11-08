66888325efd9269a7e4c390981e642ddf88441e4
Andrew-F09 <fionxia11@gmail.com>
2025-11-08 13:33:54 -0500

    added secong object (moster)
    I think it is a bit too large in my opinion, might change to a different image or scale it somehow

---
+++ Dodge-it-solo(askTeacherIfCorrectSetup)/Hero.java
@@ -0,0 +1,31 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class hero here.
+ * 
+ * Andrew (your name) 
+ * 0.0.1 (a version number or a date)
+ */
+public class Hero extends Actor
+{
+    /**
+     * Act - do whatever the hero wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    boolean atTop = true;
+    public void act()
+    {
+       //Add your action code here.
+       if(Greenfoot.mouseClicked(null)){
+          atTop =!atTop;
+       }
+       
+       if(atTop){
+           setLocation(100,250);
+        } else {
+            setLocation(300,400);
+        }
+    }
+        
+    
+}
---
+++ Dodge-it-solo(askTeacherIfCorrectSetup)/MyWorld.java
@@ -0,0 +1,27 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @Andrew 
+ * @0.0.1 (a version number or a date)
+ */
+public class MyWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        
+        Hero hero1 = new Hero();
+        addObject(hero1, 90, 67);
+        
+        Monster monster67 = new Monster();
+        addObject(monster67, 67, 67);
+    }
+}
